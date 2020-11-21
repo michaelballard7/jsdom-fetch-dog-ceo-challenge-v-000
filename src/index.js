740de993fcce828f1fc16f81828b75ce8ab5dd83
Michael Ballard <ballard.michael9@gmail.com>
2020-11-21 20:50:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-
+// add load event
 document.addEventListener('DOMContentLoaded', () => {
 
+    // select image container
 
-    let drp = document.querySelector('#breed-dropdown');
+    // select breed container 
 
-    console.log(drp.value)
+    // set images url
+
+    // set breeds url
+
+    // set images state
+
+    // set breeds state
+
+
+    // fn: imgRender add images to dom from images state
+
+    // fn: breedRender add breeds to ul
+
+    // fetch imgaes from url
+
+        // convert json to obj
+
+        // add images to state
+
+        // call img render
+
+
+    // fetch breeds from url
+
+        //  convert json to obj
+
+        // add breeds to state
+
+        // call breed render
+
+
+
+
+
 
 
 })
