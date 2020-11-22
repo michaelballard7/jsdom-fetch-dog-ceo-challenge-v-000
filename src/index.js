cf0726957d1b2877eafdc12f3fad9891208befd6
Michael Ballard <ballard.michael9@gmail.com>
2020-11-22 01:27:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -90,13 +90,14 @@ document.addEventListener('DOMContentLoaded', () => {
     // add event listener to drop down selection
     dropdown.onchange = function filterBreeds( ){
 
+            console.log('fired')
             // clear breed Container
 
             // get dropdown value
-            let filterFor = dropdown.value
+            // let filterFor = dropdown.value
 
             // filter breed state for select value and starts with
-            console.log( typeof breedsState)
+            // console.log( typeof breedsState)
 
             // call breed Render with filered state
 
