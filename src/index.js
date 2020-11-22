79db86b93aa803dc799ae76dea6bd9d0e730cb52
Michael Ballard <ballard.michael9@gmail.com>
2020-11-22 01:30:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -90,7 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // add event listener to drop down selection
     dropdown.onchange = function filterBreeds( ){
 
-            console.log(breedContainer);
+            console.log(breedContainer.children);
             // clear breed Container
             // while(breedContainer.firstChild){
             //     parent.removeChild(firstChild)
