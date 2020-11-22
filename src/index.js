a06934e3238847db5eeede756e7b8eafc6b86033
Michael Ballard <ballard.michael9@gmail.com>
2020-11-22 00:56:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .then( res => res.json())
         // add breeds to state
         .then( data =>  {
+            
             breedsState = data.message
 
             // call breed render
